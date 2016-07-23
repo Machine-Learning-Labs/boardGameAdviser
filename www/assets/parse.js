50b95ba42be14d2659ca97c63641de4fe3ba621a
meusebio.depaz <meusebio.depaz@bbva.com>
2016-07-23 18:58:47 +0200

    New DataTrainingSet

--- www/assets/parse.js
+++ www/assets/parse.js
@@ -1,44 +1,45 @@
 /*
 
-                        _              _
-                       | |            (_)
-   ___ _____   ________| |_ ___ ______ _ ___  ___  _ __
-  / __/ __\ \ / /______| __/ _ \______| / __|/ _ \| '_ \
+ _              _
+ | |            (_)
+ ___ _____   ________| |_ ___ ______ _ ___  ___  _ __
+ / __/ __\ \ / /______| __/ _ \______| / __|/ _ \| '_ \
  | (__\__ \\ V /       | || (_) |     | \__ \ (_) | | | |
-  \___|___/ \_/         \__\___/      | |___/\___/|_| |_|
-                                     _/ |
-                                    |__/
-Demo purpose file:
-usage:
-	$ npm install csv-to-json
-	$ node parse.js
-*/
+ \___|___/ \_/         \__\___/      | |___/\___/|_| |_|
+ _/ |
+ |__/
+ Demo purpose file:
+ usage:
+ $ npm install csv-to-json
+ $ node parse.js
+ */
 
 var csv = require('csv-to-json');
 var inputFile = { filename: "input.csv" || process.env.INPUT };
 
 var parse = function(resolve,reject) {
 
-	csv.parse( { filename:"training_data.csv"},
-		(err,json) => {
-			if (err) { reject(err); }
-			resolve(json);
-		});
+  csv.parse( { filename:"training_data.csv"},
+    function (err,json)  {
+      if (err) { reject(err); }
+      resolve(json);
+    });
 };
 
 var save = function(input) {
 
-	csv.writeJsonToFile({ filename: "training_data.json", json: input }, (err) => { if (err) { fail(err); }  });
+  csv.writeJsonToFile({ filename: "training_data.json", json: input },
+    function (err) { if (err) { fail(err); }  });
 };
 
 var fail = function(reason) {
 
-    console.log('Handle rejected promise ('+reason+') here.');
+  console.log('Handle rejected promise ('+reason+') here.');
 }
 
 var p1 = new Promise(parse);
-	p1.then(save)
-	  .catch(fail);
+p1.then(save)
+  .catch(fail);
 
 console.log('No olvidar: ')
 console.log('* formatear')
